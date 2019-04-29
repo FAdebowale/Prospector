2f28395ca5fe4aea6d4beca63bbf59abef23c219
Femi Adebowale <feadebowale@gmail.com>
2019-04-29 03:00:06 -0400

    Prospector Game

--- Assets/__Scripts/Prospector.cs
+++ Assets/__Scripts/Prospector.cs
@@ -10,11 +10,13 @@ public class Prospector : MonoBehaviour {
 	static public Prospector 	S;
 
 	[Header("Set in Inspector")]
-	public TextAsset			deckXML;
-
+	public TextAsset deckXML;
+    public TextAsset layoutXML;
 
 	[Header("Set Dynamically")]
-	public Deck					deck;
+	public Deck	deck;
+    public Layout layout;
+    public List<CardProspector> drawPile;
 
 	void Awake(){
 		S = this;
@@ -23,6 +25,31 @@ public class Prospector : MonoBehaviour {
 	void Start() {
 		deck = GetComponent<Deck> ();
 		deck.InitDeck (deckXML.text);
+        Deck.Shuffle(ref deck.cards); //Shuffles deck by referemce
+
+        //Card c;
+        //for(int cNum = 0; cNum < deck.cards.Count; cNum++)
+        //{
+        //    c = deck.cards[cNum];
+        //    c.transform.localPosition = new Vector3((cNum % 13) * 3, cNum / 13 * 4, 0);
+        // } 
+        layout = GetComponent<Layout>();  // Get the Layout component 
+        layout.ReadLayout(layoutXML.text); // Pass LayoutXML to it     
+
+        drawPile = ConvertListCardsToListCardProspectors(deck.cards);
 	}
+
+    List<CardProspector> ConvertListCardsToListCardProspectors(List<Card> lCD)
+    {
+        List<CardProspector> lCP = new List<CardProspector>();
+        CardProspector tCP;
+        foreach (Card tCD in lCD)
+        {
+
+            tCP = tCD as CardProspector;                                     
+            lCP.Add(tCP);
+        }
+        return (lCP);
+    }
 
 }
